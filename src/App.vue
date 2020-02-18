2c76f2aedf6af5191830cfefd3f00842fa7a26e3
Forrest Li <forrest.li@kitware.com>
2020-02-18 15:23:57 -0500

    resizable app drawer and tool button strip

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,108 @@
 <template>
   <v-app>
-    <v-app-bar
+    <resizable-nav-drawer
       app
-      color="primary"
-      dark
+      permanent
+      :min-width="200"
+      :max-width="400"
+      :width="300"
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      <v-tabs>
+        <v-tab>Data</v-tab>
+        <v-tab>Annotations</v-tab>
+      </v-tabs>
+    </resizable-nav-drawer>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+    <v-content id="content-wrapper">
+      <div class="d-flex flex-row flex-grow-1">
+        <div id="tools-strip" class="d-flex flex-column blue lighten-2">
+          <v-tooltip
+            v-for="tool in tools"
+            :key="tool"
+            right
+            transition="slide-x-transition"
+          >
+            <template v-slot:activator="{ on }">
+              <v-btn
+                class="white--text mt-1"
+                depressed
+                tile
+                color="blue lighten-2"
+                height="40"
+                width="40"
+                min-width="40"
+                max-width="40"
+                :ripple="false"
+                v-on="on"
+              >
+                <v-icon>mdi-{{ tool }}</v-icon>
+              </v-btn>
+            </template>
+            <span>{{ tool }}</span>
+          </v-tooltip>
+        </div>
+        <v-container class="d-flex flex-column flex-grow-1 pa-0">
+          <v-row no-gutters>
+            <v-col class="pa-0 view-box" cols="6">Sag</v-col>
+            <v-col class="pa-0 view-box" cols="6">3D</v-col>
+          </v-row>
+          <v-row no-gutters>
+            <v-col class="pa-0 view-box" cols="6">Cor</v-col>
+            <v-col class="pa-0 view-box" cols="6">Axi</v-col>
+          </v-row>
+        </v-container>
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import ResizableNavDrawer from './components/ResizableNavDrawer.vue';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    ResizableNavDrawer,
   },
 
   data: () => ({
-    //
+    tools: [
+      'angle-acute',
+      'ruler',
+      'pencil',
+      'crop',
+      'death-star-variant',
+      'eyedropper',
+      'fire',
+      'flask-empty-plus',
+      'gesture',
+      'layers',
+      'magnet-on',
+      'pipe-wrench',
+      'run',
+      'seat-flat-angled',
+      'white-balance-incandescent',
+    ],
   }),
 };
 </script>
+
+<style>
+#content-wrapper {
+  /* disable v-content transition when we resize our app drawer */
+  transition: initial;
+}
+
+#content-wrapper > .v-content__wrap {
+  display: flex;
+}
+
+#tools-strip {
+  flex: 0 0 40px;
+}
+
+.view-box {
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/ResizableNavDrawer.vue
@@ -0,0 +1,138 @@
+<script>
+import { VNavigationDrawer } from 'vuetify/lib';
+
+const RESIZE_CURSOR = 'ew-resize';
+
+export default {
+  name: 'ResizableNavDrawer',
+  props: {
+    minWidth: Number,
+    maxWidth: Number,
+    width: Number,
+    handleSize: {
+      type: Number,
+      default: 4,
+    },
+  },
+
+  data() {
+    return {
+      internalWidth: this.width,
+    };
+  },
+
+  computed: {
+    handleSizeCSS() {
+      return `${this.handleSize}px`;
+    },
+  },
+
+  mounted() {
+    this.oldCursor = '';
+    this.oldUserSelect = '';
+
+    this.setResizableStyles();
+    this.setEvents();
+  },
+
+  beforeDestroy() {
+    this.clearEvents();
+    this.stopResize();
+  },
+
+  render(h) {
+    // forwards slots, scoped slots, and more to v-navigation-drawer
+    return h(
+      VNavigationDrawer,
+      {
+        props: {
+          // this.$attrs is the non-prop attrs we receive, which should
+          // be forwarded to v-navigation-drawer
+          ...this.$attrs,
+          width: this.internalWidth,
+        },
+        on: this.$listeners,
+        scopedSlots: this.$scopedSlots,
+        ref: 'infoPane',
+      },
+      this.$slots.default,
+    );
+  },
+
+  methods: {
+    setResizableStyles() {
+      // manual styling
+      const el = this.$refs.infoPane.$el.querySelector('.v-navigation-drawer__border');
+
+      // NOTE: width is not reactive to handle size
+      el.style.width = this.handleSizeCSS;
+      el.style.backgroundColor = 'transparent';
+      // NOTE: dynamically setting "right" won't update the border position
+      if ('right' in this.$attrs) {
+        el.style.borderLeft = '2px solid rgba(224, 224, 224, 1)';
+      } else {
+        el.style.borderRight = '2px solid rgba(224, 224, 224, 1)';
+      }
+      el.style.cursor = RESIZE_CURSOR;
+    },
+
+    setEvents() {
+      const el = this.$refs.infoPane.$el.querySelector('.v-navigation-drawer__border');
+      el.addEventListener('mousedown', this.startResize);
+    },
+
+    clearEvents() {
+      const el = this.$refs.infoPane.$el.querySelector('.v-navigation-drawer__border');
+      el.removeEventListener('mousedown', this.startResize);
+      document.addEventListener('mouseup', this.stopResize);
+    },
+
+    startResize() {
+      this.$refs.infoPane.$el.style.transition = 'initial';
+
+      this.setCursor(RESIZE_CURSOR);
+      this.disableSelection();
+      document.addEventListener('mousemove', this.handleResize);
+      document.addEventListener('mouseup', this.stopResize);
+    },
+
+    stopResize() {
+      this.$refs.infoPane.$el.style.transition = '';
+
+      this.resetCursor();
+      this.resetSelection();
+      document.removeEventListener('mousemove', this.handleResize);
+    },
+
+    handleResize(evt) {
+      const width = 'right' in this.$attrs
+        ? document.body.scrollWidth - evt.clientX
+        : evt.clientX;
+
+      const min = this.minWidth || this.handleSize;
+      const max = this.maxWidth || Infinity;
+      this.internalWidth = Math.min(max, Math.max(min, width));
+    },
+
+    setCursor(cursor) {
+      this.oldCursor = document.body.style.cursor;
+      document.body.style.cursor = cursor;
+    },
+
+    resetCursor() {
+      document.body.style.cursor = this.oldCursor;
+      this.oldCursor = '';
+    },
+
+    disableSelection() {
+      this.oldUserSelect = document.body.style.userSelect;
+      document.body.style.userSelect = 'none';
+    },
+
+    resetSelection() {
+      document.body.style.userSelect = this.oldUserSelect;
+      this.oldUserSelect = '';
+    },
+  },
+};
+</script>
