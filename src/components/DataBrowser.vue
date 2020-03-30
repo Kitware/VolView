2f3110f21d46819318313d9f4df3caaa5726288e
Forrest Li <forrest.li@kitware.com>
2020-03-30 15:55:23 -0400

    wip(DataBrowser): Dummy data browser test

--- src/components/DataBrowser.vue
+++ src/components/DataBrowser.vue
@@ -1,51 +1,60 @@
 <template>
-  <v-expansion-panels accordion focusable id="data-browser">
-    <v-expansion-panel>
-      <v-expansion-panel-header color="#1976fa0a" class="no-select subtitle-1">
-        Patients
-      </v-expansion-panel-header>
-      <v-expansion-panel-content class="content">
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-    <v-expansion-panel>
-      <v-expansion-panel-header color="#1976fa0a" class="no-select subtitle-1">
-        Annotations
-      </v-expansion-panel-header>
-      <v-expansion-panel-content class="content">
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-    <v-expansion-panel>
-      <v-expansion-panel-header color="#1976fa0a" class="no-select subtitle-1">
-        Images
-      </v-expansion-panel-header>
-      <v-expansion-panel-content class="content">
-      </v-expansion-panel-content>
-    </v-expansion-panel>
-  </v-expansion-panels>
+  <div>
+    <item-group :value="selection" @change="setSelection">
+      <groupable-item
+        v-for="item in datasets"
+        :key="item.proxyID"
+        v-slot="{ active, select }"
+        :value="item.proxyID"
+      >
+        <v-btn
+          :color="active ? 'primary' : ''"
+          @click="select"
+        >
+          {{ item.name }}
+        </v-btn>
+      </groupable-item>
+    </item-group>
+  </div>
 </template>
 
 <script>
+import ItemGroup from '@/src/components/ItemGroup.vue';
+import GroupableItem from '@/src/components/GroupableItem.vue';
+import { mapActions, mapGetters, mapState } from 'vuex';
+
 export default {
   name: 'DataBrowser',
+
+  components: {
+    ItemGroup,
+    GroupableItem,
+  },
+
+  computed: {
+    ...mapGetters('datasets', ['selectedDataProxyID']),
+    ...mapState('datasets', {
+      datasets: (state) => state.data.map((id) => state.dataIndex[id]),
+    }),
+    selection() {
+      return this.selectedDataProxyID || null;
+    },
+  },
+
+  methods: {
+    setSelection(proxyID) {
+      if (proxyID) {
+        this.selectDataset(proxyID);
+      }
+    },
+
+    ...mapActions('datasets', ['selectDataset']),
+  },
 };
 </script>
 
 <style>
-#data-browser .v-expansion-panel--active > .v-expansion-panel-header {
-  min-height: unset;
-}
-
-#data-browser .v-expansion-panel-content__wrap {
-  padding: 0 8px;
-}
-
-#data-browser .v-expansion-panel::before {
-  box-shadow: none;
-}
 </style>
 
 <style scoped>
-#data-browser {
-  border: 1px solid rgba(0, 0, 0, 0.12);
-}
 </style>
