669627726dea99054e3dbfadf7bd2345cb18033f
Forrest Li <forrest.li@kitware.com>
2020-06-18 16:44:55 -0400

    fix(SliceSlider): Jump to slice in gutter

--- src/components/SliceSlider.vue
+++ src/components/SliceSlider.vue
@@ -2,6 +2,7 @@
   <div
     class="slice-slider"
     ref="handleContainer"
+    @mousedown="onDragStart"
   >
   <div
     class="slice-slider-handle"
@@ -10,7 +11,6 @@
       height: `${handleHeight}px`,
       transform: `translate3d(0, ${handlePosition}px, 0)`,
     }"
-    @mousedown="onDragStart"
   />
 </div>
 </template>
@@ -96,8 +96,24 @@ export default {
 
       this.dragging = true;
       this.initialMousePosY = ev.pageY;
-      const handleStyles = window.getComputedStyle(this.$refs.handle);
-      this.initialHandlePos = getYOffsetFromTransform(handleStyles.transform);
+
+      if (ev.target === this.$refs.handle) {
+        const handleStyles = window.getComputedStyle(this.$refs.handle);
+        this.initialHandlePos = getYOffsetFromTransform(handleStyles.transform);
+      } else {
+        // move handle to mouse pos
+        const { y } = this.$refs.handleContainer.getBoundingClientRect();
+        this.initialHandlePos = Math.max(
+          0,
+          Math.min(
+            this.maxHandlePos,
+            ev.pageY - y - this.handleHeight / 2,
+          ),
+        );
+        const newSlice = this.getNearestSlice(this.initialHandlePos);
+        this.$emit('input', newSlice);
+      }
+
       this.yOffset = 0;
 
       document.addEventListener('mousemove', this.onDragMove);
@@ -108,7 +124,7 @@ export default {
       ev.preventDefault();
 
       this.yOffset = ev.pageY - this.initialMousePosY;
-      const slice = this.getNearestSlice();
+      const slice = this.getNearestSlice(this.handlePosition);
       this.$emit('input', slice);
     },
 
@@ -118,12 +134,12 @@ export default {
       this.dragging = false;
       document.removeEventListener('mousemove', this.onDragMove);
       document.removeEventListener('mouseup', this.onDragEnd);
-      const slice = this.getNearestSlice();
+      const slice = this.getNearestSlice(this.handlePosition);
       this.$emit('input', slice);
     },
 
-    getNearestSlice() {
-      const sliceEstimate = this.handlePosition / this.maxHandlePos;
+    getNearestSlice(pos) {
+      const sliceEstimate = pos / this.maxHandlePos;
       const frac = sliceEstimate * (this.max - this.min) + this.min;
       return Math.round(frac / this.step) * this.step;
     },
