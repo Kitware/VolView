6c60a7371a07b73d87452248fe280b3d45c483af
Forrest Li <forrest.li@kitware.com>
2020-07-07 18:10:42 -0400

    feat(AvatarListCard): Menu slot

--- src/components/AvatarListCard.vue
+++ src/components/AvatarListCard.vue
@@ -15,10 +15,34 @@
             :src="imageUrl"
           />
         </v-col>
-        <v-col cols="8" class="text-no-wrap">
+        <v-col cols="7" class="text-no-wrap">
           <div class="ml-2">
             <slot></slot>
           </div>
+        </v-col>
+        <v-col cols="1">
+          <v-menu>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                small
+                icon
+                class="mt-n1"
+                v-bind="attrs"
+                v-on="on"
+                @mousedown.stop
+                @click.stop
+              >
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </template>
+            <slot name="menu">
+              <v-list>
+                <v-list-item disabled>
+                  No actions defined
+                </v-list-item>
+              </v-list>
+            </slot>
+          </v-menu>
         </v-col>
       </v-row>
     </v-container>
