3c741b7736add9db861f6913d6135d4f19c6ff0a
Forrest Li <forrest.li@kitware.com>
2020-07-07 22:15:16 -0400

    chore(eslint): Ignore no-named-as-default-member

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/no-named-as-default-member': 'off',
   },
 
   overrides: [
