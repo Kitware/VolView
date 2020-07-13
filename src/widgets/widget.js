d644815fc625b0a2956966724cb96596f661d19a
Forrest Li <forrest.li@kitware.com>
2020-07-13 13:39:13 -0400

    feat(widget): Support locking picking to one view

--- src/widgets/widget.js
+++ src/widgets/widget.js
@@ -9,6 +9,10 @@ function withWidgetManager(view, handler) {
   }
 }
 
+export function is2DView(view) {
+  return view.getClassName() === 'vtkView2DProxy';
+}
+
 export const FOLLOW_VIEW = 'FOLLOW_VIEW';
 export const ALWAYS_VISIBLE = 'ALWAYS_VISIBLE';
 export const NEVER_VISIBLE = 'NEVER_VISIBLE';
@@ -28,11 +32,13 @@ export default class Widget {
     this.provider = provider;
     this.watchers = [];
     this.widgetInstances = new Map();
+    this.currentView = null;
 
     // configurable
     this.mouse2DViewBehavior = ALWAYS_VISIBLE;
     this.mouse3DViewBehavior = ALWAYS_VISIBLE;
     this.removeOnDeactivate = true;
+    this.lockToCurrentViewFlag = false; // only applicable to FOLLOW_VIEW
   }
 
   watchStore(...args) {
@@ -47,6 +53,9 @@ export default class Widget {
   }
 
   removeFromView(view) {
+    if (this.currentView === view) {
+      this.currentView = null;
+    }
     withWidgetManager(view, (wm) => {
       this.widgetInstances.delete(view);
       wm.removeWidget(this.factory);
@@ -82,12 +91,61 @@ export default class Widget {
     );
   }
 
+  lockPickingToCurrentView(yn) {
+    this.lockToCurrentViewFlag = yn;
+    this.updatePicking(this.currentView);
+  }
+
+  setCurrentView(view) {
+    if (!this.lockToCurrentViewFlag) {
+      this.currentView = view;
+    }
+    this.updateManipulator(this.currentView);
+    this.updateVisibility(this.currentView);
+  }
+
+  updatePicking(currentView) {
+    const it = this.widgetInstances.entries();
+    let { value, done } = it.next();
+    while (!done) {
+      const [view, viewWidget] = value;
+      if (is2DView(view)) {
+        let pickable = true;
+        if (
+          this.mouse2DViewBehavior === FOLLOW_VIEW &&
+          this.lockToCurrentViewFlag
+        ) {
+          pickable = view === currentView;
+        }
+        viewWidget.setPickable(pickable);
+      }
+      // TODO 3D view
+
+      ({ value, done } = it.next());
+    }
+  }
+
   updateVisibility(currentView) {
     const it = this.widgetInstances.entries();
     let { value, done } = it.next();
     while (!done) {
       const [view, viewWidget] = value;
-      viewWidget.setVisibility(view === currentView);
+      if (is2DView(view)) {
+        let visible = false;
+        if (this.mouse2DViewBehavior === FOLLOW_VIEW) {
+          visible = view === currentView;
+        } else if (this.mouse2DViewBehavior === ALWAYS_VISIBLE) {
+          visible = true;
+        }
+        viewWidget.setVisibility(visible);
+        viewWidget.setContextVisibility(visible);
+      }
+      // TODO 3D view
+
+      // render
+      view.getReferenceByName('widgetManager').renderWidgets();
+      view.getRenderWindow().render();
+
       ({ value, done } = it.next());
     }
   }
