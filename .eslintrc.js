551a8d0c4e113712296ea2c1b02bca91da3b6309
forrest <forrest@sb3-kitware.local>
2022-02-17 13:07:08 -0500

    chore(eslint): typescript no-unused-vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,6 +43,9 @@ module.exports = {
     // use typescript no-shadow
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': 'error',
+    // use typescript no-unused
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
   },
 
   overrides: [
