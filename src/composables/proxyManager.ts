5f42deb4c72a30c9539c28bf72364feab8532f8b
forrest <forrest@sb3-kitware.local>
2022-02-22 15:55:38 -0500

    refactor(ts): proxyManager composable

---
+++ src/composables/proxyManager.ts
@@ -0,0 +1,13 @@
+import { inject } from '@vue/composition-api';
+import { vtkProxyManager } from '@kitware/vtk.js/Proxy/Core/ProxyManager';
+
+const defaultKey = 'ProxyManager';
+
+export function withProxyManager(cb: (pxm: vtkProxyManager) => void) {
+  const pxm = inject<vtkProxyManager>('ProxyManager');
+  return pxm ? cb(pxm) : null;
+}
+
+export function useProxyManager(key = defaultKey) {
+  return inject<vtkProxyManager>(key ?? defaultKey);
+}
