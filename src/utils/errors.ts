92d42a8aec4e1df875d6485b3c85b491701e33a6
Forrest <forrest.li@kitware.com>
2022-04-13 18:01:59 -0400

    refactor(errors): delete until we need it again

--- src/utils/errors.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export class MultipleErrors extends Error {
-  constructor(errors, msg = null) {
-    super(msg || 'Multiple errors occurred');
-    this.errors = errors;
-  }
-}
