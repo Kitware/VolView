985d96ae9a38654ccf36dd8d05c30849da02a714
Forrest <forrest.li@kitware.com>
2022-04-15 12:13:58 -0400

    fix(IJKSliceRepresentation): suppress XYZ slicing

--- src/vtk/IJKSliceRepresentationProxy/index.js
+++ src/vtk/IJKSliceRepresentationProxy/index.js
@@ -2,11 +2,27 @@ import macro from '@kitware/vtk.js/macro';
 
 import vtkSliceRepresentationProxy from '@kitware/vtk.js/Proxy/Representations/SliceRepresentationProxy';
 
+const SLICE_MODE_MAP = {
+  X: 'I',
+  Y: 'J',
+  Z: 'K',
+  I: 'I',
+  J: 'J',
+  K: 'K',
+};
+
 function vtkIJKSliceRepresentationProxy(publicAPI, model) {
   model.classHierarchy.push('vtkIJKSliceRepresentationProxy');
 
+  const superClass = { ...publicAPI };
+
   // don't set colors on slices
   publicAPI.setColorBy = () => {};
+
+  // pretend XYZ slicing is actually just IJK.
+  publicAPI.setSlicingMode = (modeString) => {
+    superClass.setSlicingMode(SLICE_MODE_MAP[modeString]);
+  };
 }
 
 // ----------------------------------------------------------------------------
