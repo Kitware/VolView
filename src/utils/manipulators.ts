e106ffe26cfe390fb08e0674de92ddafea027528
Forrest <forrest.li@kitware.com>
2022-04-15 12:05:14 -0400

    fix(manipulators): transform plane to world space

--- src/utils/manipulators.ts
+++ src/utils/manipulators.ts
@@ -1,5 +1,6 @@
 import { Vector3 } from '@kitware/vtk.js/types';
 import vtkPlaneManipulator from '@kitware/vtk.js/Widgets/Manipulators/PlaneManipulator';
+import { vec3 } from 'gl-matrix';
 import { ImageMetadata } from '../storex/datasets-images';
 import { getLPSAxisFromDir, LPSAxisDir } from './lps';
 
@@ -12,9 +13,12 @@ export function updatePlaneManipulatorFor2DView(
   const { lpsOrientation } = imageMetadata;
   const axis = lpsOrientation[getLPSAxisFromDir(viewDir)];
 
-  const normal = lpsOrientation[viewDir];
-  const origin = [0, 0, 0];
+  const normal: vec3 = lpsOrientation[viewDir];
+  const origin: vec3 = [0, 0, 0];
   origin[axis] = slice;
+
+  vec3.transformMat3(normal, normal, imageMetadata.orientation);
+  vec3.transformMat4(origin, origin, imageMetadata.indexToWorld);
 
   manipulator.setNormal(normal as Vector3);
   manipulator.setOrigin(origin as Vector3);
