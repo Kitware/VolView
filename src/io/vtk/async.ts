9024d9e2213e77a3c9b2f797be29acfb6c58b147
Forrest <forrest.li@kitware.com>
2022-05-20 16:22:04 -0400

    chore: upgrade @vue/cli

--- src/io/vtk/async.ts
+++ src/io/vtk/async.ts
@@ -1,11 +1,11 @@
 import vtk from '@kitware/vtk.js/vtk';
 
 import PromiseWorker from '@/src/utils/promise-worker';
-// eslint-disable-next-line import/extensions
-import AsyncReaderWorker from './async.worker.ts';
 
 export const runAsyncVTKReader = (readerName: string) => async (file: File) => {
-  const worker = new PromiseWorker(AsyncReaderWorker);
+  const worker = new PromiseWorker(
+    new Worker(new URL('./async.worker.ts', import.meta.url))
+  );
   const data = await worker.postMessage({
     file,
     readerName,
--- src/shims-worker.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*.worker.ts' {
-  class WebpackWorker extends Worker {
-    constructor();
-  }
-  export default WebpackWorker;
-}
