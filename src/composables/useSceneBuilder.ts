39459e4a89d72eae45fc9d7c66c4f75fe79056b1
Forrest <forrest.li@kitware.com>
2022-09-06 12:42:31 -0400

    feat(scene): load geometry into 3D view

--- src/composables/useSceneBuilder.ts
+++ src/composables/useSceneBuilder.ts
@@ -6,13 +6,15 @@ import { vtkLPSViewProxy } from '../types/vtk-types';
 interface Scene {
   baseImage?: Ref<string | null>;
   labelmaps?: Ref<string[]>;
+  models?: Ref<string[]>;
 }
 
 type RepProxy = vtkAbstractRepresentationProxy;
 
 export function useSceneBuilder<
   BaseImageType extends RepProxy = RepProxy,
-  LabelMapType extends RepProxy = RepProxy
+  LabelMapType extends RepProxy = RepProxy,
+  ModelType extends RepProxy = RepProxy
 >(viewID: string, sceneIDs: Scene) {
   const viewStore = useViewStore();
   const viewProxy = viewStore.getViewProxy<vtkLPSViewProxy>(viewID);
@@ -22,37 +24,84 @@ export function useSceneBuilder<
 
   const baseImageRep = shallowRef<BaseImageType | null>(null);
   const labelmapReps = shallowRef<LabelMapType[]>([]);
+  const modelReps = shallowRef<ModelType[]>([]);
 
-  watch(
-    () => [sceneIDs.baseImage?.value, sceneIDs.labelmaps?.value] as const,
-    ([baseImageID, labelmapIDs]) => {
-      viewProxy.removeAllRepresentations();
-
-      baseImageRep.value = null;
-      if (baseImageID) {
-        const rep = viewStore.getDataRepresentationForView<BaseImageType>(
-          baseImageID,
-          viewID
-        );
-        baseImageRep.value = rep;
-        if (rep) {
-          rep.setRescaleOnColorBy(false);
-          viewProxy.addRepresentation(rep);
-        }
-      }
-
-      labelmapReps.value = [];
-      if (labelmapIDs) {
-        labelmapIDs.forEach((id) => {
-          const rep = viewStore.getDataRepresentationForView<LabelMapType>(
-            id,
+  if (sceneIDs.baseImage) {
+    watch(
+      sceneIDs.baseImage,
+      (baseImageID) => {
+        baseImageRep.value = null;
+        if (baseImageID) {
+          const rep = viewStore.getDataRepresentationForView<BaseImageType>(
+            baseImageID,
             viewID
           );
+          baseImageRep.value = rep;
           if (rep) {
-            labelmapReps.value.push(rep);
-            viewProxy.addRepresentation(rep);
+            rep.setRescaleOnColorBy(false);
           }
-        });
+        }
+      },
+      { immediate: true }
+    );
+  }
+
+  if (sceneIDs.labelmaps) {
+    watch(
+      sceneIDs.labelmaps,
+      (labelmapIDs) => {
+        labelmapReps.value = [];
+        if (labelmapIDs) {
+          labelmapIDs
+            .map((id) =>
+              viewStore.getDataRepresentationForView<LabelMapType>(id, viewID)
+            )
+            .filter(Boolean)
+            .forEach((rep) => labelmapReps.value.push(rep!));
+        }
+      },
+      { immediate: true }
+    );
+  }
+
+  if (sceneIDs.models) {
+    watch(
+      sceneIDs.models,
+      (modelIDs) => {
+        modelReps.value = [];
+        if (modelIDs) {
+          console.log(
+            modelIDs.map((id) =>
+              viewStore.getDataRepresentationForView<ModelType>(id, viewID)
+            )
+          );
+          modelIDs
+            .map((id) =>
+              viewStore.getDataRepresentationForView<ModelType>(id, viewID)
+            )
+            .filter(Boolean)
+            .forEach((rep) => modelReps.value.push(rep!));
+        }
+      },
+      { immediate: true }
+    );
+  }
+
+  watch(
+    () => [baseImageRep.value, labelmapReps.value, modelReps.value] as const,
+    ([baseRep, lmReps, mReps]) => {
+      viewProxy.removeAllRepresentations();
+
+      if (baseRep) {
+        viewProxy.addRepresentation(baseRep);
+      }
+
+      if (lmReps?.length) {
+        lmReps.forEach((rep) => viewProxy.addRepresentation(rep));
+      }
+
+      if (mReps?.length) {
+        mReps.forEach((rep) => viewProxy.addRepresentation(rep));
       }
 
       // TODO not sure why I need this, but might as well keep
@@ -60,8 +109,9 @@ export function useSceneBuilder<
       // For reference, this doesn't get invoked when resetting the
       // camera with a supplied bounds, so we manually invoke it here.
       viewProxy.getRenderer().computeVisiblePropBounds();
+      viewProxy.render();
     },
-    { immediate: true }
+    { deep: true, immediate: true }
   );
 
   return { baseImageRep, labelmapReps };
