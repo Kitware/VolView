9b4a9f76535fa8c2e57108f29d0cc045a51d7fc9
Forrest <forrest.li@kitware.com>
2022-10-04 14:44:20 -0400

    perf(zip): use fflate for reading zips
    Tests on an 85MB zip file of DICOM data indicated that using fflate was
    overall more performant than JSZip for reading/decompression.
    - Firefox 105: ~1700ms with jszip, ~1800ms with fflate unzip()
    - Chrome 104: ~2400ms with jszip, ~1400ms with fflate unzip()
    
    Overall, a 100ms increase on Firefox is acceptable compared to a
    near 1s improvement on Chrome.

--- src/io/zip.ts
+++ src/io/zip.ts
@@ -1,31 +1,36 @@
-import JSZip from 'jszip';
+import { unzip } from 'fflate';
 import { FileEntry } from './types';
 
 export async function extractFilesFromZip(zipFile: File): Promise<FileEntry[]> {
-  const zip = await JSZip.loadAsync(zipFile);
-  const promises: Promise<File>[] = [];
-  const paths: string[] = [];
-  zip.forEach((relPath, file) => {
-    if (!file.dir) {
-      const splitPath = file.name.split('/');
-      const baseName = splitPath[splitPath.length - 1];
-      const path = [...splitPath.slice(0, splitPath.length - 1), ''].join('/');
-      const fileEntry = zip.file(file.name);
-      if (fileEntry) {
-        promises.push(
-          fileEntry.async('blob').then((blob) => new File([blob], baseName))
-        );
-        paths.push(path);
+  const ab = await zipFile.arrayBuffer();
+  return new Promise((resolve, reject) => {
+    unzip(
+      new Uint8Array(ab),
+      {
+        filter(file) {
+          return !file.name.endsWith('/');
+        },
+      },
+      (err, unzipped) => {
+        if (err) reject(err);
+        else {
+          resolve(
+            Object.entries(unzipped).map(([fullname, data]) => {
+              const splitPath = fullname.split('/');
+              const baseName = splitPath[splitPath.length - 1];
+              const path = [
+                ...splitPath.slice(0, splitPath.length - 1),
+                '',
+              ].join('/');
+              const file = new File([data], baseName);
+              return {
+                file,
+                path,
+              };
+            })
+          );
+        }
       }
-    }
-  });
-
-  return Promise.all(promises).then((files) => {
-    return files.map((file, index) => {
-      return {
-        file,
-        path: paths[index],
-      };
-    });
+    );
   });
 }
