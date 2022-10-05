646efe885d677eb0ee6a7071eb34fdb669fb10f7
Forrest <forrest.li@kitware.com>
2022-10-05 17:08:34 -0400

    refactor: remove unused code

--- src/composables/widgetProvider.js
+++
@@ -1,7 +0,0 @@
-import { inject } from '@vue/composition-api';
-
-const defaultKey = 'widgetProvider';
-
-export function useWidgetProvider(key = defaultKey) {
-  return inject(key ?? defaultKey);
-}
