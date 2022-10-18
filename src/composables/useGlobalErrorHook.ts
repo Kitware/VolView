726dac0c752e32ab931090fce53b566be8383bac
Forrest <forrest.li@kitware.com>
2022-10-18 11:48:26 -0400

    feat(App): handle uncaught errors

---
+++ src/composables/useGlobalErrorHook.ts
@@ -0,0 +1,18 @@
+import { onBeforeUnmount, onMounted } from '@vue/composition-api';
+import { useMessageStore } from '../store/messages';
+
+export function useGlobalErrorHook() {
+  const messageStore = useMessageStore();
+
+  const onError = (event: ErrorEvent) => {
+    messageStore.addError('Application error (click for details)', event.error);
+  };
+
+  onMounted(() => {
+    window.addEventListener('error', onError);
+  });
+
+  onBeforeUnmount(() => {
+    window.removeEventListener('error', onError);
+  });
+}
