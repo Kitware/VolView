d3084441cc7503dc735ad444c91b57565b4e9b06
Stephen Aylward <stephen.aylward@kitware.com>
2022-11-08 12:42:34 -0500

    STYLE: Replace <br> with mt spacing commands

--- src/components/App.vue
+++ src/components/App.vue
@@ -122,13 +122,11 @@
                         <v-icon size="64">mdi-folder-open</v-icon>
                       </div>
                       <div>Click to open local files.</div>
-                      <br /><br />
                       <div class="mt-8">
                         <v-icon size="64">mdi-arrow-down-bold</v-icon>
                       </div>
                       <div>Drag & drop your DICOM files.</div>
-                      <br /><br /><br /><br />
-                      <div class="mt-8">
+                      <div class="mt-16">
                         <v-icon size="64">mdi-cloud-off-outline</v-icon>
                       </div>
                       <div>Local mode.</div>
