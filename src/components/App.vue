921c1c80dec925d56bc58bda1b1181acb646d6f6
Stephen R. Aylward <stephen.aylward@kitware.com>
2022-11-08 16:47:38 -0500

    ENH: Post welcome message that data stays local to machine. (#215)
    Add no-cloud icon and message that data stays local on opening screen.

--- src/components/App.vue
+++ src/components/App.vue
@@ -120,11 +120,16 @@
                       <div>
                         <v-icon size="64">mdi-folder-open</v-icon>
                       </div>
-                      <div>Click anywhere here to open files</div>
+                      <div>Click to open local files.</div>
                       <div class="mt-8">
                         <v-icon size="64">mdi-arrow-down-bold</v-icon>
                       </div>
-                      <div>Drop your files anywhere here to open</div>
+                      <div>Drag &amp; drop your DICOM files.</div>
+                      <div class="mt-16">
+                        <v-icon size="64">mdi-cloud-off-outline</v-icon>
+                      </div>
+                      <div>Local mode.</div>
+                      <div>Data stays on your machine.</div>
                     </v-card>
                   </v-row>
                 </v-col>
