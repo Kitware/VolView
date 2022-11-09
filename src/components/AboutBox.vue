e707ae87007b16fc9737195218644121f53f653a
Stephen R. Aylward <stephen.aylward@kitware.com>
2022-11-09 00:15:49 -0500

    ENH: Updated About box, with an image and proper license info. (#224)

--- src/components/AboutBox.vue
+++ src/components/AboutBox.vue
@@ -4,8 +4,23 @@
       <vol-view-full-logo />
     </v-card-title>
     <v-card-text>
+      <p align="center">
+        <img
+          src="../assets/KitwareHeadAndNeck.jpg"
+          alt="Head and neck CT rendering"
+          style="width: 200px"
+          align="center"
+        /><br />
+      </p>
       <p>
-        VolView is an open-source web application developed at
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://volview.kitware.com/"
+        >
+          VolView
+        </a>
+        is an open-source web application developed at
         <a
           rel="noopener noreferrer"
           target="_blank"
@@ -13,37 +28,59 @@
         >
           Kitware
         </a>
-        for visualizing and annotating medical images stored in DICOM, NRRD,
-        NIFTI, and other file formats. It is an application built on top of the
-        <a
-          rel="noopener noreferrer"
-          target="_blank"
-          href="https://github.com/InsightSoftwareConsortium/itk-wasm/"
-        >
-          itk-wasm
-        </a>
-        and
-        <a
-          rel="noopener noreferrer"
-          target="_blank"
-          href="https://github.com/Kitware/vtk-js"
-        >
-          vtk.js
-        </a>
-        toolkits.
+        for visualizing and annotating medical images. It key features include:
       </p>
-      <!-- prettier-ignore -->
-      <p>
-        Want help in customizing VolView for your next application?
-        <a
-          rel-="noopener noreferrer"
-          target="_blank"
-          href="https://www.kitware.com/contact/project/"
-        >Contact us</a>!
-      </p>
-      <ul class="mt-3">
+
+      <ul>
+        <li>Fast: Drag-and-drop DICOM files for quick viewing</li>
         <li>
-          Source code:
+          Beautiful: Cinematic volume rendering to generate high-quality 3D
+          visualizations
+        </li>
+        <li>
+          Friendly: Familiar radiological image visualization and annotation
+          tools
+        </li>
+        <li>
+          Secure: Your data stays on your machine. No cloud services or data
+          servers are used.
+        </li>
+      </ul>
+      <br />
+      VolView is freely available for research, educational, and commercial
+      applications. It is built using a variety of open-source toolks created by
+      Kitware, such as
+      <a
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://github.com/InsightSoftwareConsortium/itk-wasm/"
+      >
+        itk-wasm
+      </a>
+      for DICOM I/O and image processing, and
+      <a
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://github.com/Kitware/vtk-js"
+      >
+        vtk.js
+      </a>
+      for in-browser scientific visualization.
+      <br />
+      <br />
+      Want help customizing VolView or creating a new web-based visualization
+      application?
+      <a
+        rel-="noopener noreferrer"
+        target="_blank"
+        href="https://www.kitware.com/contact/project/"
+        >Contact Kitware!</a
+      >
+      <br />
+      <br />
+      VolView source code:
+      <ul>
+        <li>
           <a
             rel="noopener noreferrer"
             target="_blank"
@@ -52,8 +89,11 @@
             https://github.com/KitwareMedical/VolView
           </a>
         </li>
+      </ul>
+      <br />
+      VolView bug reports and feature requests:
+      <ul>
         <li>
-          Bug reports and feature requests:
           <a
             rel="noopener noreferrer"
             target="_blank"
@@ -63,20 +103,19 @@
           </a>
         </li>
       </ul>
-      <!-- prettier-ignore -->
-      <p> <br>
-        Sample data provided by the following sources:
-      </p>
-      <ul class="mt-3">
+      <br />
+      Sample data provided by the following sources:
+      <ul>
         <li>
-          PROSTATEx Challenge Data (<a
+          PROSTATEx Challenge Data: Geert Litjens, Oscar Debats, Jelle Barentsz,
+          Nico Karssemeijer, and Henkjan Huisman. "ProstateX Challenge data",
+          The Cancer Imaging Archive (2017). DOI: 10.7937/K9TCIA.2017.MURS5CL
+          (<a
             rel="noopener noreferrer"
             target="_blank"
             href="https://wiki.cancerimagingarchive.net/pages/viewpage.action?pageId=23691656"
             >TCIA</a
-          >) : Geert Litjens, Oscar Debats, Jelle Barentsz, Nico Karssemeijer,
-          and Henkjan Huisman. "ProstateX Challenge data", The Cancer Imaging
-          Archive (2017). DOI: 10.7937/K9TCIA.2017.MURS5CL
+          >)
         </li>
         <li>
           MRA Head and Neck: Test case 98890234 from the "Patient Contributed
@@ -86,6 +125,15 @@
             href="http://www.pcir.org/researchers/98890234_20030505_MR.html"
             >PCIR.org</a
           >)
+        </li>
+        <li>
+          3D ultrasound of a fetus: Sample data from
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="http://tomovision.comi"
+            >tomovision.com</a
+          >
         </li>
       </ul>
     </v-card-text>
