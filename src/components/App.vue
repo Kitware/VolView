ced589c70c7fea06c85896da484cb5efbf65beb9
Stephen R. Aylward <stephen.aylward@kitware.com>
2022-11-10 18:10:47 -0500

    ENH: Add additional space between data-loading options and secure note

--- src/components/App.vue
+++ src/components/App.vue
@@ -125,11 +125,11 @@
                         <v-icon size="64">mdi-arrow-down-bold</v-icon>
                       </div>
                       <div>Drag &amp; drop your DICOM files.</div>
+                      <br /> <br /> <br />
                       <div class="mt-16">
                         <v-icon size="64">mdi-cloud-off-outline</v-icon>
                       </div>
-                      <div>Local mode.</div>
-                      <div>Data stays on your machine.</div>
+                      <div>Secure: Your data never leaves your machine.</div>
                     </v-card>
                   </v-row>
                 </v-col>
