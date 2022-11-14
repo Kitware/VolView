f5e9dc3bac7c1f40c1fd1b946042758256e60387
Stephen R. Aylward <stephen.aylward@kitware.com>
2022-11-14 13:23:36 -0500

    STYLE: Prettify

--- src/components/AboutBox.vue
+++ src/components/AboutBox.vue
@@ -104,8 +104,8 @@
         </li>
       </ul>
       <br />
-      This work was funded, in part, by the NIH via NIBIB and NIGMS
-      R01EB021396, NIBIB R01EB014955, NCI R01CA220681, and NINDS R42NS086295
+      This work was funded, in part, by the NIH via NIBIB and NIGMS R01EB021396,
+      NIBIB R01EB014955, NCI R01CA220681, and NINDS R42NS086295
       <br />
       <br />
       Sample data provided by the following sources:
