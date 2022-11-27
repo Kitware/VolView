f79d57f82e3b2be4773a171d259e08820c5281a6
Forrest <forrest.li@kitware.com>
2022-11-27 12:09:34 -0500

    fix(SliceSlider): use pointer events

--- src/components/SliceSlider.vue
+++ src/components/SliceSlider.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="slice-slider" ref="handleContainer" @mousedown="onDragStart">
+  <div
+    class="slice-slider"
+    ref="handleContainer"
+    @pointerdown="onDragStart"
+    @pointermove="onDragMove"
+    @pointerup="onDragEnd"
+    @pointercancel="onDragEnd"
+  >
     <div class="slice-slider-track" />
     <div
       class="slice-slider-handle"
@@ -115,11 +122,11 @@ export default {
 
       this.yOffset = 0;
 
-      document.addEventListener('mousemove', this.onDragMove);
-      document.addEventListener('mouseup', this.onDragEnd);
+      this.$refs.handleContainer.setPointerCapture(ev.pointerId);
     },
 
     onDragMove(ev) {
+      if (!this.$refs.handleContainer.hasPointerCapture(ev.pointerId)) return;
       ev.preventDefault();
 
       this.yOffset = ev.pageY - this.initialMousePosY;
@@ -128,11 +135,11 @@ export default {
     },
 
     onDragEnd(ev) {
+      if (!this.$refs.handleContainer.hasPointerCapture(ev.pointerId)) return;
       ev.preventDefault();
+      this.$refs.handleContainer.releasePointerCapture(ev.pointerId);
 
       this.dragging = false;
-      document.removeEventListener('mousemove', this.onDragMove);
-      document.removeEventListener('mouseup', this.onDragEnd);
       const slice = this.getNearestSlice(this.handlePosition);
       this.$emit('input', slice);
     },
@@ -147,6 +154,10 @@ export default {
 </script>
 
 <style scoped>
+.slice-slider {
+  touch-action: none;
+}
+
 .slice-slider-handle {
   position: relative;
   width: 100%;
