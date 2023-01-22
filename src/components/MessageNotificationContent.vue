367185b770634524c666de6e77309be828636e4a
Forrest <forrest.li@kitware.com>
2023-01-21 23:35:59 -0500

    fix(MessageNotifications): add details button
    The details button gives users a visual indicator to click on the
    notification for more information.

---
+++ src/components/MessageNotificationContent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <span>{{ message }}</span>
+    &nbsp;
+    <v-btn v-if="detailsButton" dark depressed small color="#00000022">
+      details
+    </v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  props: {
+    message: String,
+    detailsButton: Boolean,
+  },
+});
+</script>
