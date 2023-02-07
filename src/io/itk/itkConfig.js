27b701669422aeb92c6beaaae141a7c89aa66abb
Paul Elliott <paul.elliott@kitware.com>
2023-02-07 12:08:57 -0500

    fix(itkConfig): handle no VUE_APP_PUBLIC_PATH provided case

--- src/io/itk/itkConfig.js
+++ src/io/itk/itkConfig.js
@@ -1,8 +1,9 @@
+const base = process.env.VUE_APP_PUBLIC_PATH ?? '';
 const itkConfig = {
-  pipelineWorkerUrl: `${process.env.VUE_APP_PUBLIC_PATH}/itk/web-workers/min-bundles/pipeline.worker.js`,
-  imageIOUrl: `${process.env.VUE_APP_PUBLIC_PATH}/itk/image-io`,
-  meshIOUrl: `${process.env.VUE_APP_PUBLIC_PATH}/itk/mesh-io`,
-  pipelinesUrl: `${process.env.VUE_APP_PUBLIC_PATH}/itk/pipelines`,
+  pipelineWorkerUrl: `${base}/itk/web-workers/min-bundles/pipeline.worker.js`,
+  imageIOUrl: `${base}/itk/image-io`,
+  meshIOUrl: `${base}/itk/mesh-io`,
+  pipelinesUrl: `${base}/itk/pipelines`,
 };
 
 export default itkConfig;
