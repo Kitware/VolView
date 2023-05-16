b540058713a0826eb83bdedad850e593f22b8499
Forrest <forrest.li@kitware.com>
2023-05-16 18:01:46 -0400

    style: delete unused code

--- src/store/view-configs/common.ts
+++ src/store/view-configs/common.ts
@@ -1,5 +1,4 @@
 import { DoubleKeyRecord } from '@/src/utils/doubleKeyRecord';
-import { useDoubleRecord } from '@/src/composables/useDoubleRecord';
 import { StateFile, ViewConfig } from '../../io/state-file/schema';
 import {
   CameraConfig,
@@ -9,23 +8,6 @@ import {
   WindowLevelConfig,
 } from './types';
 import { ensureDefault } from '../../utils';
-
-class ReturnTypeOfDoubleRecord<T> {
-  Return = useDoubleRecord<T>();
-}
-type ConfigMap = ReturnTypeOfDoubleRecord<any>['Return'];
-
-export const removeViewFromConfig = (config: ConfigMap) => (viewID: string) =>
-  config.deleteFirstKey(viewID);
-
-export const removeDataFromConfig =
-  (config: ConfigMap) => (dataID: string, viewID?: string) => {
-    if (viewID) {
-      config.delete(viewID, dataID);
-    } else {
-      config.deleteSecondKey(dataID);
-    }
-  };
 
 type SubViewConfig =
   | CameraConfig
