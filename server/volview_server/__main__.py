15b28bdf8c233f0e9fc3e1849c64d5a9c1dfcd10
Forrest <forrest.li@kitware.com>
2023-06-12 16:44:03 -0400

    feat(server): support verbose and msg size args

--- server/volview_server/__main__.py
+++ server/volview_server/__main__.py
@@ -7,6 +7,8 @@ from aiohttp import web
 
 from volview_server.rpc_server import RpcServer
 
+MAX_HTTP_BUFFER_SIZE = 4 * 1024 * 1024 * 1024
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -15,6 +17,15 @@ def parse_args():
     )
     parser.add_argument(
         "-P", "--port", default=4014, help="Port for server to listen on"
+    )
+    parser.add_argument(
+        "--max-message-size",
+        type=int,
+        default=MAX_HTTP_BUFFER_SIZE,
+        help="Max message size. Set to 0 for the max allowable size.",
+    )
+    parser.add_argument(
+        "--verbose", default=False, action="store_true", help="Enable verbose logging."
     )
     parser.add_argument("api_script", help="Python file that exposes ServerApi")
     return parser.parse_args()
@@ -37,14 +48,25 @@ def run_server(ApiClass, *, host: str, port: int, **kwargs):
 
 def main(args):
     ApiClass = load_api_script(args.api_script)
+
+    max_http_buffer_size = args.max_message_size
+    if max_http_buffer_size == 0:
+        max_http_buffer_size = sys.maxsize
+
+    if args.verbose:
+        print(f"Using a max message size of {max_http_buffer_size}")
+
     run_server(
         ApiClass,
         host=args.host,
         port=args.port,
-        # RpcServer kwargs
+        # AsyncServer kwargs
         async_mode="aiohttp",
         async_handlers=True,
         cors_allowed_origins="*",
+        logger=args.verbose,
+        engineio_logger=args.verbose,
+        max_http_buffer_size=max_http_buffer_size,
     )
 
 
