d4888e678dc5e4f72c9a1df5778f5cafd468b963
Paul Elliott <paul.elliott@kitware.com>
2023-07-31 15:06:32 -0400

    feat(DataBrowser): hide Sample Data if configured
    Closes #380

--- src/io/import/processors/handleConfig.ts
+++ src/io/import/processors/handleConfig.ts
@@ -3,36 +3,46 @@ import { z } from 'zod';
 import { ImportHandler } from '@/src/io/import/common';
 import { useRectangleStore } from '@/src/store/tools/rectangles';
 import { useRulerStore } from '@/src/store/tools/rulers';
+import { useDataBrowserStore } from '@/src/store/data-browser';
 
-const Color = z.string();
+const color = z.string();
 
-const Label = z.object({
-  color: Color,
+const label = z.object({
+  color,
 });
 
-const RulerLabel = Label;
+const rulerLabel = label;
 
-const RectangleLabel = z.intersection(
-  Label,
+const rectangleLabel = z.intersection(
+  label,
   z.object({
-    fillColor: Color,
+    fillColor: color,
   })
 );
 
-const Config = z.object({
-  labels: z.record(Label).or(z.null()).optional(),
-  rulerLabels: z.record(RulerLabel).or(z.null()).optional(),
-  rectangleLabels: z.record(RectangleLabel).or(z.null()).optional(),
+const dataBrowser = z
+  .object({
+    hideSampleData: z.boolean().optional(),
+  })
+  .optional();
+
+const config = z.object({
+  labels: z.record(label).or(z.null()).optional(),
+  rulerLabels: z.record(rulerLabel).or(z.null()).optional(),
+  rectangleLabels: z.record(rectangleLabel).or(z.null()).optional(),
+  dataBrowser,
 });
+
+type Config = z.infer<typeof config>;
 
 const readConfigFile = async (configFile: File) => {
   const decoder = new TextDecoder();
   const ab = await configFile.arrayBuffer();
   const text = decoder.decode(new Uint8Array(ab));
-  return Config.parse(JSON.parse(text));
+  return config.parse(JSON.parse(text));
 };
 
-const applyConfig = (manifest: z.infer<typeof Config>) => {
+const applyLabels = (manifest: Config) => {
   // pass through null labels, use fallback labels if undefined
   const labelsIfUndefined = (toolLabels: typeof manifest.labels) => {
     if (toolLabels === undefined) return manifest.labels;
@@ -40,6 +50,15 @@ const applyConfig = (manifest: z.infer<typeof Config>) => {
   };
   useRulerStore().mergeLabels(labelsIfUndefined(manifest.rulerLabels));
   useRectangleStore().mergeLabels(labelsIfUndefined(manifest.rectangleLabels));
+};
+
+const applySampleData = (manifest: Config) => {
+  useDataBrowserStore().hideSampleData = !!manifest.dataBrowser?.hideSampleData;
+};
+
+const applyConfig = (manifest: Config) => {
+  applyLabels(manifest);
+  applySampleData(manifest);
 };
 
 /**
