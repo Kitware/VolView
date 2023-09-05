d15b65aa4bbf9219554cbadd74fe24504a2f8488
Paul Elliott <paul.elliott@kitware.com>
2023-09-05 15:57:45 -0400

    feat(PolygonWidget): show bounding box upon hover

---
+++ src/components/tools/BoundingRectangle.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { useViewStore } from '@/src/store/views';
+import { worldToSVG } from '@/src/utils/vtk-helpers';
+import { nonNullable } from '@/src/utils/index';
+import vtkLPSView2DProxy from '@/src/vtk/LPSView2DProxy';
+import vtkBoundingBox from '@kitware/vtk.js/Common/DataModel/BoundingBox';
+import { Bounds, Vector3 } from '@kitware/vtk.js/types';
+import { computed, ref, watch, toRefs } from 'vue';
+import { onVTKEvent } from '@/src/composables/onVTKEvent';
+
+const props = defineProps<{
+  points: Array<Vector3>;
+  viewId: string;
+}>();
+
+const viewStore = useViewStore();
+const viewProxy = computed(
+  () => viewStore.getViewProxy<vtkLPSView2DProxy>(props.viewId)!
+);
+
+const visible = computed(() => {
+  return props.points.length > 0;
+});
+
+const rectangle = ref({
+  x: 0,
+  y: 0,
+  width: 0,
+  height: 0,
+});
+
+const updateRectangle = () => {
+  const viewRenderer = viewProxy.value.getRenderer();
+
+  const screenBounds = [...vtkBoundingBox.INIT_BOUNDS] as Bounds;
+  props.points
+    .map((point) => {
+      const point2D = worldToSVG(point, viewRenderer);
+      return point2D;
+    })
+    .filter(nonNullable)
+    .forEach(([x, y]) => {
+      vtkBoundingBox.addPoint(screenBounds, x, y, 0);
+    });
+  const [x, y] = vtkBoundingBox.getMinPoint(screenBounds);
+  const [maxX, maxY] = vtkBoundingBox.getMaxPoint(screenBounds);
+  rectangle.value = {
+    x,
+    y,
+    width: maxX - x,
+    height: maxY - y,
+  };
+};
+
+const { points } = toRefs(props);
+watch([points], updateRectangle, { immediate: true, deep: true });
+
+onVTKEvent(viewProxy, 'onModified', updateRectangle)
+</script>
+
+<template>
+  <rect v-if="visible" :x="rectangle.x" :y="rectangle.y" :width="rectangle.width" :height="rectangle.height"
+    stroke-width="2" fill="transparent" stroke="lightgray" />
+</template>
--- src/components/tools/polygon/PolygonTool.vue
+++ src/components/tools/polygon/PolygonTool.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="overlay-no-events">
     <svg class="overlay-no-events">
+      <bounding-rectangle :points="pointsToBound" :view-id="viewId" />
       <polygon-widget-2D
         v-for="tool in tools"
         :key="tool.id"
@@ -16,11 +17,7 @@
       />
     </svg>
     <annotation-context-menu ref="contextMenu" :tool-store="activeToolStore" />
-    <annotation-info
-      class="overlay-no-events"
-      :info="overlayInfo"
-      :tool-store="activeToolStore"
-    />
+    <annotation-info :info="overlayInfo" :tool-store="activeToolStore" />
   </div>
 </template>
 
@@ -53,6 +50,7 @@ import {
 } from '@/src/composables/annotationTool';
 import AnnotationContextMenu from '@/src/components/tools/AnnotationContextMenu.vue';
 import AnnotationInfo from '@/src/components/tools/AnnotationInfo.vue';
+import BoundingRectangle from '@/src/components/tools/BoundingRectangle.vue';
 import PolygonWidget2D from './PolygonWidget2D.vue';
 
 type ToolID = PolygonID;
@@ -83,6 +81,7 @@ export default defineComponent({
     PolygonWidget2D,
     AnnotationContextMenu,
     AnnotationInfo,
+    BoundingRectangle,
   },
   setup(props) {
     const { viewDirection, currentSlice } = toRefs(props);
@@ -195,6 +194,12 @@ export default defineComponent({
       currentSlice
     );
 
+    const pointsToBound = computed(() => {
+      if (!overlayInfo.value.visible) return [];
+      const tool = activeToolStore.toolByID[overlayInfo.value.toolID];
+      return tool.points;
+    });
+
     return {
       tools: currentTools,
       placingToolID,
@@ -204,6 +209,7 @@ export default defineComponent({
       activeToolStore,
       onHover,
       overlayInfo,
+      pointsToBound,
     };
   },
 });
