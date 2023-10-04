438283d8d4f771423d62b1900c13d66a1f3282cb
Forrest <forrest.li@kitware.com>
2023-10-04 11:35:56 -0400

    fix(types): export proper vtk types

--- src/vtk/LabelMapSliceRepProxy/index.d.ts
+++ src/vtk/LabelMapSliceRepProxy/index.d.ts
@@ -3,4 +3,10 @@ import vtkIJKSliceRepresentationProxy from '../IJKSliceRepresentationProxy';
 export interface vtkLabelMapSliceRepProxy
   extends vtkIJKSliceRepresentationProxy {}
 
+export function extend(publicAPI: object, model: object): void;
+
+export declare const vtkLabelMapSliceRepProxy: {
+  extend: typeof extend;
+};
+
 export default vtkLabelMapSliceRepProxy;
