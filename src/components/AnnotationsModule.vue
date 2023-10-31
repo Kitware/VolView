a8987f302657524253dfddc17dbf7938054769b1
Forrest <forrest.li@kitware.com>
2023-10-31 16:12:03 -0400

    feat(AnnotationsModule): use tabs for annotations

--- src/components/AnnotationsModule.vue
+++ src/components/AnnotationsModule.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
-import { AnnotationToolType } from '@/src/store/tools/types';
+import { ref, watch } from 'vue';
+import { storeToRefs } from 'pinia';
+import { AnnotationToolType, Tools } from '@/src/store/tools/types';
+import { useToolStore } from '@/src/store/tools';
 import MeasurementsToolList from './MeasurementsToolList.vue';
 import LabelmapControls from './LabelmapControls.vue';
 import ToolControls from './ToolControls.vue';
 import MeasurementRulerDetails from './MeasurementRulerDetails.vue';
 
-const tools = [
+const Tabs = {
+  Measurements: 'measurements',
+  Labelmaps: 'labelmaps',
+};
+
+const MeasurementTools = [
   {
     type: AnnotationToolType.Ruler,
     icon: 'mdi-ruler',
@@ -20,31 +28,53 @@ const tools = [
     icon: 'mdi-pentagon-outline',
   },
 ];
+
+const MeasurementToolTypes = new Set<string>(
+  MeasurementTools.map(({ type }) => type)
+);
+
+const tab = ref('measurements');
+const { currentTool } = storeToRefs(useToolStore());
+
+function autoFocusTab() {
+  if (currentTool.value === Tools.Paint) {
+    tab.value = Tabs.Labelmaps;
+  } else if (MeasurementToolTypes.has(currentTool.value)) {
+    tab.value = Tabs.Measurements;
+  }
+}
+
+watch(
+  currentTool,
+  () => {
+    autoFocusTab();
+  },
+  { immediate: true }
+);
 </script>
 
 <template>
   <div class="overflow-y-auto mx-2 fill-height">
     <tool-controls />
-    <div class="header">Measurements</div>
-    <div class="content">
-      <measurements-tool-list :tools="tools" />
-    </div>
-    <div class="text-caption text-center empty-state">No measurements</div>
-    <div class="header">Labelmaps</div>
-    <div class="content">
-      <labelmap-controls />
-    </div>
-    <div class="text-caption text-center empty-state">No labelmaps</div>
+    <v-divider />
+    <v-tabs v-model="tab" align-tabs="center" density="compact" class="my-1">
+      <v-tab value="measurements" class="tab-header">Measurements</v-tab>
+      <v-tab value="labelmaps" class="tab-header">Labelmaps</v-tab>
+    </v-tabs>
+    <v-window v-model="tab">
+      <v-window-item value="measurements">
+        <measurements-tool-list :tools="MeasurementTools" />
+      </v-window-item>
+      <v-window-item value="labelmaps">
+        <labelmap-controls />
+      </v-window-item>
+    </v-window>
   </div>
 </template>
 
 <style scoped>
-.empty-state {
-  display: none;
-}
-
-.content:empty + .empty-state {
-  display: block;
+.tab-header {
+  font-size: 0.8rem;
 }
 </style>
 
