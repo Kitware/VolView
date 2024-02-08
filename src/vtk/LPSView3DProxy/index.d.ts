badc75e00d862cd7c70768322cbc5cceeefeb67e
Forrest <forrest.li@kitware.com>
2024-02-08 13:18:14 -0500

    fix(LPSView3DProxy): prioritize LPSViewProxyBase

--- src/vtk/LPSView3DProxy/index.d.ts
+++ src/vtk/LPSView3DProxy/index.d.ts
@@ -2,7 +2,7 @@ import { vec3 } from 'gl-matrix';
 import vtkViewProxy from '@kitware/vtk.js/Proxy/Core/ViewProxy';
 import { LPSViewProxyBase } from '@/src/vtk/LPSViewProxyBase';
 
-export interface vtkLPSView3DProxy extends vtkViewProxy, LPSViewProxyBase {}
+export interface vtkLPSView3DProxy extends LPSViewProxyBase, vtkViewProxy {}
 
 export function extend(publicAPI: object, model: object): void;
 
