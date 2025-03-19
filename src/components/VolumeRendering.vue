9b2e77b6fd81d3914ff1a38577701ff3cb01a385
Forrest <forrest.li@kitware.com>
2025-03-19 13:46:12 -0400

    fix(volumeColoring): watching a non-ref object

--- src/components/VolumeRendering.vue
+++ src/components/VolumeRendering.vue
@@ -135,7 +135,7 @@ export default defineComponent({
       if (!animationRequested) {
         animationRequested = true;
         viewAnimationStore.requestAnimation(pwfWidget, {
-          byViewType: ['3D'],
+          byViewType: [TARGET_VIEW_ID],
         });
       }
     };
