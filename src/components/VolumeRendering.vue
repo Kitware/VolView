b374d205d158f2e636937bc5619b39f34e649375
Forrest <forrest.li@kitware.com>
2025-03-19 13:40:04 -0400

    chore: update vue + vueuse and remove patches

--- src/components/VolumeRendering.vue
+++ src/components/VolumeRendering.vue
@@ -184,7 +184,8 @@ export default defineComponent({
       (image) => {
         if (image) {
           const scalars = image.getPointData().getScalars();
-          pwfWidget.setDataArray(scalars);
+          // TODO(fli): setDataArray() re-computes the data range unnecessarily
+          pwfWidget.setDataArray(scalars.getData());
           pwfWidget.render();
         }
       },
