bf6d000e7120520cf4b202fb2cd65f8b7b60142d
Forrest <forrest.li@kitware.com>
2025-08-21 10:59:59 -0400

    fix(LayoutGridItem): show overlay w/o image

--- src/components/LayoutGridItem.vue
+++ src/components/LayoutGridItem.vue
@@ -28,7 +28,7 @@ const activeStyles = computed(() => {
     };
   }
   return {
-    border: '2px solid black',
+    border: '2px solid rgba(255, 255, 255, 0.05)',
   };
 });
 
@@ -68,13 +68,38 @@ function onDrop(event: DragEvent) {
 
 <template>
   <div
+    class="grid-item"
     :style="activeStyles"
     @dragenter="onDragEnter"
     @dragleave="onDragLeave"
     @drop="onDrop"
   >
+    <div v-show="!imageID" class="overlay">
+      <v-icon color="grey-darken-3" size="x-large">mdi-image-off</v-icon>
+    </div>
     <CurrentImageProvider :image-id="imageID">
       <ItemComponent :view-id="viewId" />
     </CurrentImageProvider>
   </div>
 </template>
+
+<style scoped>
+.grid-item {
+  position: relative;
+  box-sizing: border-box;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 10;
+  user-select: none;
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
